791e7bd8aa18e83e3b367847c779db1ce8682895
Pk117464 <92030719+Pk117464@users.noreply.github.com>
2021-11-10 14:40:39 +0100

    strings_vars.py

---
+++ vars.py
@@ -0,0 +1,50 @@
+a = 257293579275927597125972589723489578957679676813460813468234609238902578345723458234
+print(type(a))
+b = 10
+c = a + b
+print('a: {}'.format(a))
+print('c: {}'.format(c))
+
+d = 6347347437473474747.635645326
+d= 257293579275927597125972589723489578957679676813460813468234609238902578345723458234.0
+print(type(d))
+
+e = 10.0
+e= 257293579275927597125972589723489578957679676813460813468234609238902578345723458234.0
+print(type(e))
+f = d + e
+print('f: {}'.format(f))
+
+bb=0b101
+print(bb)
+xx = 0x101
+print(xx)
+xx = 0xf1
+print(xx)
+
+a1 = 1349
+b1 = 353
+c1= a1/b1
+print('c1: ' + str(c1))
+print('c1: {}'.format(c1))
+c2 = int(c1)
+print ('c2: {}'.format(c2))
+c3= round(c1,2)
+print('c2: {}'.format(c3))
+
+s = '3463.63'
+print('type of s: {}'.format(type(s)))
+f1= float(s)
+print(type(f1))
+
+
+s1='101'
+#Interpret as hexadecimal
+h_s1= int(s1, 16)
+print(h_s1)
+
+#Interpret as binary
+b_s1 = int(s1,2)
+print(b_s1)
+
+
