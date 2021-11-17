63c9d29722e1a8afe429b76065fac09b1e9e2b08
Pk117464 <92030719+Pk117464@users.noreply.github.com>
2021-11-17 19:11:47 +0100

    strings_vars.py

---
+++ conditions_loops.py
@@ -0,0 +1,20 @@
+p= True
+q= False
+
+if p and q:
+    print('p and q')
+else:
+    print('not p and q')
+
+if p and q:
+    print('p and q')
+
+
+if p:
+     v=10
+else:
+     v=20
+print(v)
+
+v=10 if p else 20
+print(v)
---
+++ loops.py
@@ -0,0 +1,15 @@
+
+
+print('____')
+for i in range(8):
+    if i==3:
+        continue
+    if i==6:
+         break
+    print(i)
+print('_______')
+a= 1
+while a <17:
+    print(a)
+    a = a + a
+
